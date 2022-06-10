ac82f2029da2805fcdc47ab36e32e74061c62d2c
mrz-f0Th <rakazzakaria22@gmail.com>
2022-06-10 13:18:53 +0700

    tampilan mobile | desktop

--- src/components/layout/Layout.vue
+++ src/components/layout/Layout.vue
@@ -1,15 +1,36 @@
 <template>
-  <div class="bg-gray-200 min-h-screen hidden md:block">
+  <div class="bg-gray-200 min-h-screen overflow-hidden hidden md:block">
     <Navbar />
     <router-view />
   </div>
+
+  <div class="md:hidden bg-gray-200">
+    <Navbar />
+    <Jumbotron />
+    <AboutMe />
+    <Porto />
+    <Skills />
+    <ContactMe />
+  </div>
 </template>
 
-        <script>
+<script>
 import Navbar from "../Navbar.vue";
+import Jumbotron from "../Jumbotron.vue"
+import AboutMe from "../AboutMe.vue"
+import Porto from "../Porto.vue"
+import Skills from "../Skills.vue"
+import ContactMe from "../Contact.vue"
 
 export default {
-  components: { Navbar },
+  components: { 
+      Navbar,
+      Jumbotron,
+      AboutMe,
+      Porto,
+      Skills,
+      ContactMe
+    },
 };
 </script>
 
