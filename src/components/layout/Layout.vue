e338b8a4c259a81f2edaa44914c6f1ba2e1a6adc
mrz-f0Th <rakazzakaria22@gmail.com>
2022-06-10 13:11:57 +0700

    test breakpoint

--- src/components/layout/Layout.vue
+++ src/components/layout/Layout.vue
@@ -1,30 +1,11 @@
 <template>
-<div class="bg-gray-200 min-h-screen">
-  <Navbar />
-  <router-view />
-  <!-- <section class="py-10 md:py-16">
-
-        <div class="container max-w-screen-xl mx-auto px-4">
-
-            <div class="text-center">
-                <h1 class="font-normal text-gray-300 text-3xl md:text-6xl lg:text-7xl mb-20 md:mb-32 lg:mb-48">Please do
-                    not measure your skills in <br> percentages!</h1>
-
-                <p class="font-medium text-gray-700 text-xs md:text-base">In my many years of experience, I use @laravel
-                    for backend projects and @vuejs for <br> front-end projects. I’m an avid programmer, so I create
-                    designs based on the <br> weekend @figmadesign.</p>
-            </div>
-
-        </div>
-
-    </section> -->
-
-  
-</div>
+  <div class="bg-gray-200 min-h-screen hidden md:block">
+    <Navbar />
+    <router-view />
+  </div>
 </template>
 
         <script>
-
 import Navbar from "../Navbar.vue";
 
 export default {
