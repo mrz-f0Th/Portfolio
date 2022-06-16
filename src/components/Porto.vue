d9c7035a7b8fd0738d4b4afdecfc9365a7533e49
mrz-f0Th <72551171+mrz-f0Th@users.noreply.github.com>
2022-06-16 11:11:04 +0700

    Update Porto.vue
    Cv

--- src/components/Porto.vue
+++ src/components/Porto.vue
@@ -41,19 +41,7 @@
                         </div>
                     </div>
 
-                    <div class="flex space-x-6">
-                        <h1 class="font-normal text-gray-700 text-3xl md:text-4xl">03</h1>
-
-                        <span class="w-28 h-0.5 bg-gray-300 mt-5"></span>
-
-                        <div>
-                            <h1 class="font-normal text-gray-700 text-3xl md:text-4xl mb-5">Demo API Generator</h1>
-
-                            <p class="font-normal text-gray-500 text-sm md:text-base">A dummy data free and documented
-                                API generator to facilitate <br> the process of testing the front-end portion of
-                                projects.</p>
-                        </div>
-                    </div>
+    
                 </div>
             </div>
 
